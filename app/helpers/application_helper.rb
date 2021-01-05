f1c57d393c8592ae85889206d750b60e944b1304
Justin Robinson <justinorobinson@icloud.com>
2021-01-05 02:55:34 -0800

    enable sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def sign_in_url
+    cociety = Rails.application.config.cociety
+    protocol = cociety[:protocol] == :http ? URI::HTTP : URI::HTTPS
+    protocol.build(
+      host:  cociety[:host],
+      port:  cociety[:port],
+      path:  cociety[:sign_in_path],
+      query: { redirect_to: request.original_url }.to_query
+    ).to_s
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,5 @@
 require 'active_support/core_ext/integer/time'
 
-beacon_cociety_local = 'beacon.cociety.local'
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -16,7 +14,14 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  config.hosts << ENV.fetch('HOST', beacon_cociety_local)
+  config.host = ENV.fetch('HOST', 'beacon.cociety.local')
+  config.cociety = {
+    host:         ENV.fetch('COCIETY_HOST', 'cociety.local'),
+    port:         ENV.fetch('COCIETY_PORT', 3000),
+    protocol:     :http,
+    sign_in_path: '/customer/sign_in'
+  }
+  config.hosts << config.host
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
