3851e710829d2cf44af0a4a9317ea7a4c8201e2a
Justin Robinson <justinorobinson@icloud.com>
2021-01-05 23:04:58 -0800

    add heroku app.json for tests

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,5 @@ module Beacon
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.assets.initialize_on_precompile = false
   end
 end
