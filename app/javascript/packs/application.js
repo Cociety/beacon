5f4ab12518b0f13fbde53e89006b366fa80d7ddb
Justin Robinson <justinorobinson@icloud.com>
2021-01-07 03:40:49 -0800

    include CSRF token with calls made to same second level domain

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,32 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+Rails.secondLevelDomain = function(host) {
+  return host.split(':')[0].split('.').slice(-2).join('.');
+}
+
+Rails.isSameSecondLevelDomain = function (originAnchor, urlAnchor) {
+  return Rails.secondLevelDomain(urlAnchor.host) === Rails.secondLevelDomain(originAnchor.host);
+}
+
+Rails._isCrossDomain = Rails.isCrossDomain;
+/**
+ * Modified to allow matching second level domains.
+ * Used by rails to determine if a url is crossDomain before it adds a CSRF token to ajax requests
+ * @Override
+ * @param {string} url
+ */
+Rails.isCrossDomain = function (url) {
+  const originAnchor = document.createElement('a');
+  originAnchor.href = location.href;
+  const urlAnchor = document.createElement('a');
+  urlAnchor.href = url;
+
+  try {
+    return !Rails.isSameSecondLevelDomain(originAnchor, urlAnchor) && Rails._isCrossDomain(url);
+  } catch {
+    // any errors assume cross domain, just like original method
+    return true;
+  }
+}
