cbbfdace51485ab9d5528f1a13880398a1d6aec0
Justin Robinson <justinorobinson@icloud.com>
2021-01-09 04:50:22 -0800

    install hotwire and make goal respond to changes

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 5.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
@@ -40,7 +40,7 @@ group :development do
   gem 'web-console', '>= 4.1.0'
   # Display performance information such as SQL time and flame graphs for each request in your browser.
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
-  gem 'rack-mini-profiler', '~> 2.0'
+  # gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring-watcher-listen', '~> 2.0.0'
@@ -59,3 +59,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "devise", "~> 4.7"
+
+gem "hotwire-rails", "~> 0.1.2"
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,16 +1,29 @@
 class Goal < ApplicationRecord
   default_scope { order(created_at: :asc) }
 
-  scope :children, -> { left_outer_joins(:parent_relationships).merge(Relationship.where.not child_id: nil) }
+  after_create_commit { replace_parent }
+  after_update_commit { replace_parent }
+  before_destroy { @parent = top_level_parent }
+  after_destroy_commit { @parent.broadcast_replace_to 'goals' }
+  
   scope :parents, -> { left_outer_joins(:parent_relationships).merge(Relationship.where child_id: nil) }
-
   has_many :parent_relationships, foreign_key: :child_id, class_name: :Relationship, inverse_of: :child
   has_many :parents, through: :parent_relationships, inverse_of: :children, dependent: :destroy
-
+  
+  scope :children, -> { left_outer_joins(:parent_relationships).merge(Relationship.where.not child_id: nil) }
   has_many :child_relationships, foreign_key: :parent_id, class_name: :Relationship, inverse_of: :parent
   has_many :children, through: :child_relationships, inverse_of: :parents, dependent: :destroy
 
   def child?
     parents.count.positive?
+  end
+
+  def replace_parent
+    top_level_parent.broadcast_replace_to 'goals'
+  end
+
+  def top_level_parent
+    return parents.first.top_level_parent if child?
+    self
   end
 end
