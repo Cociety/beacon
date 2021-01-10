6048e4e828a4907ce9c0c8dfb3f84f161aa2b7d5
Justin Robinson <justinorobinson@icloud.com>
2021-01-10 00:36:04 -0800

    add ability to delete a goal relationship without deleting the goal

---
+++ test/models/goal/relationship_test.rb
@@ -0,0 +1,17 @@
+class Goal::RelationshipTest < ActiveSupport::TestCase
+  setup do
+    @parent = goals(:parent1)
+  end
+
+  test 'should delete a relationship' do
+    assert_difference -> { Goal::Relationship.count }, -1 do
+      @parent.child_relationships.first.delete
+    end
+  end
+
+  test 'should destroy relationships' do
+    assert_difference -> { Goal::Relationship.count }, -1 do
+      @parent.child_relationships.first.destroy
+    end
+  end
+end
