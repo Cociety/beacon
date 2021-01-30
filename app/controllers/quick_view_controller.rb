a87df7a3636350b02f821a65d1e81883d6ade216
Justin Robinson <justinorobinson@icloud.com>
2021-01-30 00:47:42 -0800

    quick_view goal route

---
+++ app/controllers/quick_view_controller.rb
@@ -0,0 +1,7 @@
+class QuickViewController < ApplicationController
+  before_action :set_goal, only: [:index]
+
+  def set_goal
+    @goal = Goal.find(params[:goal_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :customers, skip: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :goals, only: [:update, :destroy] do
+  resources :goals, only: %i[update destroy] do
+    resources :quick_view, only: %i[index]
     match 'adopt/:new_child_id', action: :adopt, via: %i[put patch]
   end
 end
