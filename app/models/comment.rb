da6b983c197e058808b5e8d23560dbeb9143e4b6
Justin Robinson <justinorobinson@icloud.com>
2021-02-04 02:17:22 -0800

    render latest comments oldest to newest top to bottom

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
-  after_create_commit { broadcast_prepend_to :comments }
+  after_create_commit { broadcast_append_to :comments }
 
   alias_attribute :by, :customer
   belongs_to :commentable, polymorphic: true
