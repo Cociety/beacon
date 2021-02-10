aa6c6b0e5afd7033d3a8d5a3c16000b205932c71
Justin Robinson <justinorobinson@icloud.com>
2021-02-09 23:20:48 -0800

    make cociety records read only again

--- app/models/cociety_record.rb
+++ app/models/cociety_record.rb
@@ -2,4 +2,8 @@ class CocietyRecord < ApplicationRecord
   self.abstract_class = true
 
   connects_to database: { writing: :cociety, reading: :cociety }
+
+  def readonly?
+    true
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,5 @@
 class Customer < CocietyRecord
-  # Include default devise modules. Others available are:
-  # :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable,
-         timeout_in: 2.weeks
-
+  # We sign in with Cociety by going to sign_in_url and sign_out_url and being redirected back to Beacon
+  devise :database_authenticatable, :timeoutable, timeout_in: 2.weeks
   has_person_name
 end
