540caf49e6994192a7e904c3a7959590b007f443
Justin Robinson <justinorobinson@icloud.com>
2021-02-18 00:59:18 -0800

    redirect to trees index if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
   before_action :skip_authorization
+  before_action :redirect_to_trees_if_logged_in
+
+  private
+
+  def redirect_to_trees_if_logged_in
+    redirect_to trees_path if customer_signed_in?
+  end
 end
