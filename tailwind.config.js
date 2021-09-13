858e2a1f71e66598f400bf342565961de933b314
Justin Robinson <justinorobinson@icloud.com>
2021-09-12 21:04:11 -0400

    enable tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 module.exports = {
+  mode: 'jit',
   purge: {
-    content: ['./app/views/**/*.html.erb'],
+    content: [
+      './app/views/**/*.html.erb'
+    ],
     enabled: true,
-    options: {
-      safelist: [
-        "type", // [type='checkbox']
-      ],
-    },
+    safelist: [
+      'type', // [type='checkbox']
+      // goal states dynamically assigned in tree_controller.js
+      'assigned',
+      'in_progress',
+      'testing',
+      'done',
+      'blocked',
+      // classess assigned in tree_controller.js
+      'label',
+      'avatar',
+      'link',
+      'node'
+    ],
     preserveHtmlElements: true
   },
   darkMode: false, // or 'media' or 'class'
