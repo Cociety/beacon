d15fd7f2b53e340d2c68b449ceb8b9c1fd8d8b33
Justin Robinson <justinorobinson@icloud.com>
2021-09-26 00:37:24 -0400

    only bind puma to all interfaces in development

--- config/puma.rb
+++ config/puma.rb
@@ -11,14 +11,15 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
+rails_devo = ENV.fetch('RAILS_ENV', 'development') == 'development'
+worker_timeout 3600 if rails_devo
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port ENV.fetch('PORT')
 
 # listen to all interfaces
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT')}"
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT')}" if rails_devo
 
 # Specifies the `environment` that Puma will run in.
 #
