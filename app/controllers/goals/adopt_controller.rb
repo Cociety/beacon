d9db0c79e7618bccb5cb836ed1a1d5322288ef19
Justin Robinson <justinorobinson@icloud.com>
2022-01-18 21:25:44 -0500

    space tree previews and always reload page during goal adoption

--- app/controllers/goals/adopt_controller.rb
+++ app/controllers/goals/adopt_controller.rb
@@ -5,7 +5,7 @@ class Goals::AdoptController < ApplicationController
     @new_child = authorize Goal.find(params[:id])
     @goal.toggle_adoption @new_child
     @to = goal_path(@goal)
-    render_js 'redirect'
+    render_js 'reload'
   rescue
     flash[:alert] = 'Failed to adopt goal'
     render_js 'reload'
@@ -16,6 +16,7 @@ class Goals::AdoptController < ApplicationController
   def render_js(view)
     respond_to do |format|
       format.js { render view }
+      format.json { redirect_to @goal }
     end
   end
 end
