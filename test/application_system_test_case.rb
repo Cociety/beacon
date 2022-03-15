6988e0a4469a1b50816ea2ef91e988cf1051fe8f
Justin Robinson <justinorobinson@icloud.com>
2022-03-14 20:04:29 -0400

    disable VCR intercepting system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,12 @@
 require 'test_helper'
 
+VCR.configure do |config|
+  config.ignore_request do |request|
+    host = URI(request.uri).host
+    ['chromedriver.storage.googleapis.com', '127.0.0.1'].include? host
+  end
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
