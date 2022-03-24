289e31c918109113779c76ef1efa1687540d9e71
Justin Robinson <justinorobinson@icloud.com>
2022-03-23 21:41:02 -0400

    suffix RAILS_ENV to HTML title on non production pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def goal_states
     Goal.states.map { |state, _| OpenStruct.new(value: state, text: t(state)) }
   end
+
+  def title
+    "Beacon#{ENV['RAILS_ENV'] == 'production' ? '' : " [#{ENV['RAILS_ENV']}]"}"
+  end
 end
